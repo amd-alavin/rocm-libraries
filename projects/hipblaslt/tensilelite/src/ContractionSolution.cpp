f3b3ab460ab4ceb9c412509c684da9a99f18f044
Seung-Hee Bae <Seung-Hee.Bae@amd.com>
2026-06-11 19:13:07 -0400

    [hipblaslt] place the batch offset argument at the tail of the kernarg buffer

--- projects/hipblaslt/tensilelite/src/ContractionSolution.cpp
+++ projects/hipblaslt/tensilelite/src/ContractionSolution.cpp
@@ -992,17 +992,6 @@ namespace TensileLite
                                           autoGsuVal,
                                           ntab);
 
-        // Batch offset support for General Batched GEMM (SupportUserArgs kernels)
-        // Placed after core GEMM args (strides, alpha/beta, StreamK) to match kernel signature
-        //if(problemType.supportDeviceUserArguments && !problemType.groupedGemm)
-        if(!problemType.groupedGemm)
-        {
-            args.template append<int64_t>("batchOffsetD", inputs.batchOffsetD);
-            args.template append<int64_t>("batchOffsetC", inputs.batchOffsetC);
-            args.template append<int64_t>("batchOffsetA", inputs.batchOffsetA);
-            args.template append<int64_t>("batchOffsetB", inputs.batchOffsetB);
-        }
-
 	// NOTE: an assumption here is A & B must be both MX data types or non-MX data types.
 	//       Mixing is not supported.
         if(!problemType.useScaleAB.empty())
@@ -1122,16 +1111,6 @@ namespace TensileLite
                                               (uint8_t*)inputs.ws + workspaceOffsetInByte);
             args.template append<const void*>("AmaxSync", inputs.Synchronizer);
         }
-
-        // Batch offset support for General Batched GEMM (SupportUserArgs kernels)
-        // Placed at the end of the parameter list
-//        if(!problemType.groupedGemm)
-//        {
-//            args.template append<int64_t>("batchOffsetD", inputs.batchOffsetD);
-//            args.template append<int64_t>("batchOffsetC", inputs.batchOffsetC);
-//            args.template append<int64_t>("batchOffsetA", inputs.batchOffsetA);
-//            args.template append<int64_t>("batchOffsetB", inputs.batchOffsetB);
-//        }
     }
 
     inline uint32_t getNumWorkGroups(const KernelInvocation& rv)
@@ -1870,6 +1849,16 @@ namespace TensileLite
             rv.args.append<uint32_t>("GSUSync", 0);
         }
 
+        // Batch offset support for General Batched GEMM (SupportUserArgs kernels).
+        // Appended at the tail, after the dstD/Synchronizer block, to match the
+        // kernel signature order (see Signature.py).
+        if(!problemType.groupedGemm)
+        {
+            rv.args.append<int64_t>("batchOffsetD", inputs.batchOffsetD);
+            rv.args.append<int64_t>("batchOffsetC", inputs.batchOffsetC);
+            rv.args.append<int64_t>("batchOffsetA", inputs.batchOffsetA);
+            rv.args.append<int64_t>("batchOffsetB", inputs.batchOffsetB);
+        }
 
         if(problemType.stochasticRounding)
         {
@@ -2149,13 +2138,6 @@ namespace TensileLite
             rv.args.append<void const*>("C", inputs.c);
         else
             rv.args.append<void const* const*>("batchC", inputs.batchC);
-
-        // Pass batch offsets when kernel expects them (SupportUserArgs=true, not GroupedGemm)
-//        if(problemType.supportDeviceUserArguments && !problemType.groupedGemm)
-//        {
-//            rv.args.append<int64_t>("batchOffsetD", inputs.batchOffsetD);
-//            rv.args.append<int64_t>("batchOffsetC", inputs.batchOffsetC);
-//        }
 
         if(problemType.useBias && sizeMapping.globalAccumulation == 0 && (!problemType.useGradient))
         {
